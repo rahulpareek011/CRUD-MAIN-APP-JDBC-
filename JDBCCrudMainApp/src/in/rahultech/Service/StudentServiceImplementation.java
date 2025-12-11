17e5c7bf66a8c8f5fcc55967fda3f4c0f435970c
rahul pareek <itsrahul615@gmail.com>
2025-12-11 18:22:51 +0530

    StudentServiceImplementation class again asks to dao for data

---
+++ JDBCCrudMainApp/src/in/rahultech/Service/StudentServiceImplementation.java
@@ -0,0 +1,35 @@
+package in.rahultech.Service;
+
+import in.rahultech.daofactory.StudentDaoFactory;
+import in.rahultech.dto.Student;
+import in.rahultech.persistence.RStudentDao;
+import in.rahultech.persistence.StudentDaoImplementation;
+
+public class StudentServiceImplementation implements RStudentService {
+
+	RStudentDao daoService;
+	public String addStudent(Integer sId,String sName, Integer sAge, String sAddress) {
+		daoService = StudentDaoFactory.getStudentDao();//new StudentDaoImplementation();
+		String msg = daoService.addStudent(sId,sName, sAge, sAddress);
+		return msg;
+	}
+
+	@Override
+	public Student searchStudent(Integer sid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateStudent(Integer sid, String sname, Integer sage, String saddress) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteStudent(Integer sid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
