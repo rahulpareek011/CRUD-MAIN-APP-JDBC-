05eb07f488c84ba450c96105486f8172c0115360
rahul pareek <itsrahul615@gmail.com>
2025-12-11 18:16:30 +0530

    This class giving singleton object for StudentServiceImplementation

--- JDBCCrudMainApp/src/in/rahultech/serviceFactory/StudentServiceFactory.java
+++ JDBCCrudMainApp/src/in/rahultech/serviceFactory/StudentServiceFactory.java
@@ -5,11 +5,12 @@ import in.rahultech.Service.StudentServiceImplementation;
 
 //Connection connection = DriverManager.getConnection(URL,userName,password);
 public class StudentServiceFactory {
-	//make constructor private to avoid object creation
+	//make constructor private to avoid object creation from outside
 	private StudentServiceFactory() {}
 	
 	private static RStudentService studentservice = null;
 	
+	//logic to create only one object
 	public static RStudentService getStudentService() {
 		//Singleton Design pattern code
 		if(studentservice == null) {
